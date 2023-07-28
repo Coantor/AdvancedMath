027a3ebcd730af2252147552475ed8aee0ca13ac
cantour123 <3513947744@qq.com>
2023-07-28 20:27:11 +0800

    多项式实现,使用R语言最新的R6面向对象系统

---
+++ 高等代数/R语言实现多项式/多项式类.R
@@ -0,0 +1,126 @@
+library(R6)
+Poly <- R6Class(
+  classname = "Poly", #定义一个多项式类
+
+  public = list(
+    vec = NA,
+    deg = NA,
+    initialize  = function(vec){
+      self$vec = vec #比如vec=c(1,2,3),则x^2 + 2* x + 3
+      if(length(vec) == 1 & vec[1] == 0){  
+        self$deg = -Inf #否则定义为零多项式
+      }else {
+        self$deg = length(vec) - 1 #正常的次数定义
+      }
+    },
+    ## 泛函
+    print = function(...) {
+      if(self$deg == -Inf){
+        cat("0\n")
+      }else{
+      strs = paste("x^","(",collapses = 0:self$deg,"):",sep = "")
+      cat(paste(strs,sep = "  "), '\n')
+      cat(paste(round(self$vec,3),collapse = "      "),"\n")
+      }
+    }
+)
+)
+mutiply = function(one,ano_poly){
+  m = one$deg + 1
+  n = ano_poly$deg + 1
+  new_ploy = Poly$new(0)
+  if(m != -Inf & n != -Inf)
+  { 
+    new_ploy$deg =m + n- 2 #公式
+    #计算多项式的系数
+    vec = rep(0, m + n-1) #保存新的向量
+    for(i in 1:m){
+      for(j in 1:n){
+        vec[i+j-1] = vec[i+j-1] + one$vec[i] * ano_poly$vec[j]
+      }
+    }
+    new_ploy$vec = vec
+  }
+  return(new_ploy)
+}
+plus = function(one,ano_poly){
+  # 加法原则创建新的多项式
+  m = one$deg + 1
+  n = ano_poly$deg + 1
+  if(m == -Inf){
+    return(ano_poly)
+  }else if (n == -Inf){
+    return(one)
+  }
+  new_ploy = Poly$new(rep(1,max(m,n)))
+  
+  if(m >= n){
+    new_ploy$vec = one$vec + c(ano_poly$vec,rep(0,m-n))
+  }else{
+    new_ploy$vec = ano_poly$vec + c(one$vec,rep(0,n-m))
+  }
+  # 检查最大项是否为0
+  if(all(new_ploy$vec  == 0)){
+    return(Poly$new(0)) #放回0多项式
+  }
+  while( new_ploy$vec[new_ploy$deg+1] == 0 ){
+    new_ploy$vec = new_ploy$vec[1:new_ploy$deg]
+    new_ploy$deg = new_ploy$deg - 1
+  }
+  return(new_ploy)
+} # 加法函数
+mins = function(one,ano_poly){
+  if(one$deg == -Inf){
+    temp_poly = ano_poly$clone()
+    temp_poly$vec = (-1)*temp_poly$vec
+    return(temp_poly)
+  }else if(ano_poly$deg == -Inf){
+    return(one)
+  }
+  temp_poly = ano_poly$clone()
+  temp_poly$vec = (-1)*temp_poly$vec
+  new_ploy = plus(one,temp_poly)
+  return(new_ploy)
+}#减法函数
+
+
+division = function(one,ano_poly){
+  m = one$deg + 1
+  n = ano_poly$deg + 1 
+  if(m == -Inf | n == -Inf){
+    stop("除法的特殊情况")
+  }
+  if(m < n){
+    q_poly = Poly$new(0)
+    r_poly = one
+    }
+  else{
+    vec_q = rep(0,m-n+1)
+    last_kx = one$clone()
+    for(i in (m-n+1):1){
+      vec_q[i] = last_kx$vec[i-1+n]/ano_poly$vec[n]
+      last_kx = mins(last_kx,mutiply(Poly$new(vec_q),ano_poly)) #计算剩下的系数
+      if(all(last_kx$vec == 0)){
+        break;
+      }
+    }
+    q_poly = Poly$new(vec_q)
+    r_poly = mins(one,mutiply(ano_poly ,q_poly))
+  }
+  return(list(qx = q_poly,rx = r_poly))
+} #带余除法,迭代算法.返回列表
+
+#求最大公因式
+Max_CommonFactor <- function(one,ano_poly){
+  Q = division(one,ano_poly)
+  #qx = Q[[1]]
+  rx = Q[[2]]
+  qx = ano_poly
+  while(any(rx$vec != 0)){
+    Q  = division(qx,rx)
+    qx = rx
+    rx = Q[[2]]
+  }
+  qx$vec = qx$vec/qx$vec[length(qx$vec)] #首一多项式
+  return(qx)
+}
---
+++ 高等代数/R语言实现多项式/多项式类测试脚本.R
@@ -0,0 +1,26 @@
+source("多项式类.R")  #产生一个多项式类
+
+### 代码测试
+
+p1 = Poly$new(c(1,2,3))
+p2 = Poly$new(c(4,5,123,90))
+## 乘法
+p3 = mutiply(p1,p2)
+print(p3)
+## 加法
+p4 = plus(p2,p1)
+print(p4)
+## 减法
+p5 = mins(p1,p2)
+print(p5)
+
+## 带余除法
+Q = division(p3,p1)
+print(Q[[1]])
+
+print(Q[[2]])
+
+## 最大公因式
+q1 = Poly$new(c(-1,2,-1,-1,1))
+q2 = Poly$new(c(1,-2,0,1))
+rx = Max_CommonFactor(q1,q2)
