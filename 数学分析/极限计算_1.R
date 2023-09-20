55751d02abbd7b0409575bb7f3727fc024f098dd
Cantour123 <118971507+Coantor@users.noreply.github.com>
2023-09-20 23:07:21 +0800

    提交Fouriner分析的代码

--- 数学分析/极限计算_1.R
+++ 数学分析/极限计算_1.R
@@ -15,3 +15,14 @@ plot(n,value,ylim = c(0.4,1.4))
 abline(h = sqrt(pi/2),col = "red")
 library(ggplot2)
 xlim()
+
+## 绘制复旦2023第一题
+x = seq(-2,2,len = 1000)
+z = 1:length(x)
+for(i in 1:length(x)){
+  op = c(x[i],x[i]+1)
+  oa = c(x[i]-1,x[i]+1)
+  z[i] = sum(op*oa)/sqrt(sum(oa*oa)*sum(op*op))
+}
+plot(x,acos(z),cex = 0.3)
+abline(h = pi/4,col = "red")
